a305b19bea02643f2312aad9c1e7aed8db220014
Anurag Das <anurag.das23.ad@gmail.com>
2021-05-29 20:27:07 -0400

    API Server Setup

---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const app = require('./app');
+
+const PORT = prcoess.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}...`);
+});
+
+
+
