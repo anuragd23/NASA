48d2480c812c2e73bbfb5b11d4b2288e226ddb8f
Anurag Das <anurag.das23.ad@gmail.com>
2021-05-31 22:19:15 -0400

    Adding data access layer

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -30,9 +30,13 @@ function loadPlanetsData() {
         });
     });
 }
+
+function getAllPlanets() {
+    return habitablePlanets;
+}
   
 
 module.exports = {
     loadPlanetsData,
-    planets: habitablePlanets,
+    getAllPlanets,
 };
