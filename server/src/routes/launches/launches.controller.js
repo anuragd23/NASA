12a73cbc1627cf1bbd13b549cbca2747ae90f0a1
Anurag Das <anurag.das23.ad@gmail.com>
2021-06-16 09:05:42 -0400

    Paginating the endpoints

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -3,10 +3,14 @@ const {
     getAllLaunches, 
     scheduleNewLaunch, 
     abortLaunchWithId, 
-} = require('../../models/launches.model')
+} = require('../../models/launches.model');
+
+const { getPagination } = require('../../services/query'); 
 
 async function httpGetAllLaunches(req, res) {
-    return res.status(200).json(await getAllLaunches());
+    const { skip, limit } = getPagination(req.query);
+    const launches = await getAllLaunches(skip, limit);
+    return res.status(200).json(launches);
 }
 
 
